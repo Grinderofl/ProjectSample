36c9c22637c85ecaead5963ac4b42a9054c11428
Nero Sule <sulenero@gmail.com>
2016-06-08 07:50:49 +0100

    Covers other path of execution

--- ProjectSample.UnitTests/Domain/CustomerSpecs.cs
+++ ProjectSample.UnitTests/Domain/CustomerSpecs.cs
@@ -58,5 +58,49 @@ namespace ProjectSample.UnitTests.Domain
             }
         }
 
+        public class WhenTransferringBasketToACustomerWithItemsAlreadyInBasket : CustomerContext
+        {
+            private Customer _originalCustomer;
+            private Customer _customerToTransfer;
+
+            protected override void Context()
+            {
+                _originalCustomer = new Customer();
+                _customerToTransfer = new Customer();
+                var product1 = new Product()
+                {
+                    Id = 1,
+                    Name = "Foo"
+                };
+
+                var product2 = new Product()
+                {
+                    Id = 2,
+                    Name = "Bar"
+                };
+
+                _originalCustomer.Basket.Add(product1, 1);
+
+                _customerToTransfer.Basket.Add(product1, 2);
+                _customerToTransfer.Basket.Add(product2, 3);
+            }
+
+            protected override void Because()
+            {
+                _originalCustomer.TransferBasket(_customerToTransfer);
+            }
+
+            [Test]
+            public void should_contain_right_number_of_products()
+            {
+                _originalCustomer.Basket.Items.Count().Should().Be(2);
+            }
+
+            [Test]
+            public void should_contain_right_number_of_items()
+            {
+                _originalCustomer.Basket.Items.Sum(x => x.Quantity).Should().Be(6);
+            }
+        }
     }
 }
